fa435dfcded6719f028d7f88a5058b768a4df871
AcctAsmaa <asom022@gmail.com>
2025-07-08 16:44:46 +0300

    fix: resolve  database conflict

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -7,10 +7,5 @@ export const databaseConfig: DataSourceOptions = {
     'postgresql://neondb_owner:npg_EeHQN8O1Bcsa@ep-jolly-shadow-a8v62je4-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require',
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   synchronize: true,
-<<<<<<< HEAD
-
-  //synchronize: process.env.DB_SYNCHRONIZE === 'true',
-=======
->>>>>>> c9cf8b70cc7a6b8ef7942e48e3b7438764347bf5
 };
 
---
+++ src/modules/purchase-order/entities/product.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  sku: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'numeric', precision: 12, scale: 2 })
+  price: number;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+
+ 
+}
