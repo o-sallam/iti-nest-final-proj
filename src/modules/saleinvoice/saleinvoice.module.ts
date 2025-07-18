c76f8da97a1333d0f56bab09613c22809b9df2d3
o-sallam <omar.sallam.dev@gmail.com>
2025-07-18 03:47:24 +0200

    fix(importing the inventory to the salinvoice module)

--- src/modules/saleinvoice/saleinvoice.module.ts
+++ src/modules/saleinvoice/saleinvoice.module.ts
@@ -5,9 +5,10 @@ import { SaleInvoiceService } from './saleinvoice.service';
 import { SaleInvoice } from './saleinvoice.entity';
 import { SaleInvoiceItem } from './saleinvoice-item.entity';
 import { Client } from '../client/client.entity';
+import { Inventory } from '../inventory/inventory.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SaleInvoice, SaleInvoiceItem, Client])],
+  imports: [TypeOrmModule.forFeature([SaleInvoice, SaleInvoiceItem, Client, Inventory])],
   providers: [SaleInvoiceService],
   controllers: [SaleInvoiceController],
 })
