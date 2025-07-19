29a0d35908befacf31fefe6252e0866738516bab
o-sallam <omar.sallam.dev@gmail.com>
2025-07-19 02:27:20 +0200

    fix decorator

--- src/modules/saleinvoice/saleinvoice.dto.ts
+++ src/modules/saleinvoice/saleinvoice.dto.ts
@@ -1,12 +1,29 @@
-export class CreateSaleInvoiceDto {
-  warehouseId: number;
-  clientId: number;
-  paid: number;
-  items: CreateSaleInvoiceItemDto[];
-}
+import { IsNumber, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateSaleInvoiceItemDto {
+  @IsNumber()
   productId: number;
+
+  @IsNumber()
   quantity: number;
+
+  @IsNumber()
   salePrice: number;
+}
+
+export class CreateSaleInvoiceDto {
+  @IsNumber()
+  warehouseId: number;
+
+  @IsNumber()
+  clientId: number;
+
+  @IsNumber()
+  paid: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateSaleInvoiceItemDto)
+  items: CreateSaleInvoiceItemDto[];
 }
